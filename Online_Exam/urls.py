1f5041fc7a0df2a5ca579e9b0910a21f9a85137f
TonyChen9305 <gmvup4xjp6@gmail.com>
2019-12-12 15:04:12 +0800

    Viewer:can use the url show_given_exam

--- Online_Exam/urls.py
+++ Online_Exam/urls.py
@@ -91,8 +91,8 @@ urlpatterns = [
     url(r'^score$', scores.index, name='all_exam_score_list'),
     url(r'^score/', include([
         url(r'^pie$', views.pie_viewer, name='score_pie'),
-        # url(r'^(?P<exam_id>[0-9]+)$', scores.show_given_exam, name='show_given_exam'),
-        # url(r'^testee/(?P<user_id>[0-9]+)$', scores.show_given_testee, name='show_given_testee'),
+        url(r'^exam/(?P<exam_id>[0-9]+)$', scores.show_given_exam, name='show_given_exam'),
+        url(r'^testee/(?P<user_id>[0-9]+)$', scores.show_given_testee, name='show_given_testee'),
     ])),
 
     url(r'^tester$', scores.tester_index, name='testee_exam_score'),
