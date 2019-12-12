61898d6f2c8af8c882cc77cc585ef030a2357d6a
TonyChen9305 <gmvup4xjp6@gmail.com>
2019-12-12 15:04:12 +0800

    Viewer:can use the url show_given_exam

--- alcpt/scores.py
+++ alcpt/scores.py
@@ -27,9 +27,13 @@ def index(request):
     }
 
     num_pages, answer_sheets_all = viewer.query_answer_sheet(**keywords, page=page)
+    answer_sheets = []
+    for sheet in answer_sheets_all:
+        if sheet.exam.testpaper.is_testpaper == 1:
+            answer_sheets.append(sheet)
     exam_ids = []
     exams = []
-    for sheet in answer_sheets_all:
+    for sheet in answer_sheets:
         if exam_ids:
             is_exist = 0
             for exam_id in exam_ids:
@@ -44,7 +48,7 @@ def index(request):
         exams.append({'exam_id': exam_id, 'name': 'one_exam', 'average_score': 0, 'finish_time': '2019-05-19 22:21:43.643000',
                       'total': 0, 'frequency': 0,})
 
-    for sheet in answer_sheets_all:
+    for sheet in answer_sheets:
         for exam in exams:
             if exam['exam_id'] == sheet.exam.id:
                 exam['name'] = sheet.exam.name
@@ -112,3 +116,26 @@ def sheet_detail(request, exam_id):
         question_dir[que_id]['correct'] = Question.objects.get(id=que_id).correct
 
     return render(request, 'score/score_details.html', locals())
+
+@permission_check(UserType.Viewer)
+@require_http_methods(["GET"])
+def show_given_exam(request, exam_id):
+    from .models import AnswerSheet
+    answer_sheets_all = AnswerSheet.objects.all()
+    answer_sheets = []
+    for sheet in answer_sheets_all:
+        if sheet.exam.testpaper.is_testpaper == 1:
+            answer_sheets.append(sheet)
+    exams = []
+    for exam in answer_sheets:
+        if exam.exam_id == int(exam_id):
+            exams.append({'exam_id': exam_id, 'name': exam.exam.name, 'score': exam.score, 'finish_time': exam.finish_time,
+                          'testee': exam.user.user.name, 'testee_id': exam.user_id,})
+    data = {'exams': exams,
+            'exam_id': exam_id,}
+    return render(request, 'score/show_given_exam.html', data)
+
+@permission_check(UserType.Viewer)
+@require_http_methods(["GET"])
+def show_given_testee(request, user_id):
+    return render(request, 'score/show_given_exam.html')
