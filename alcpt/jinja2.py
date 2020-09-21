9380f9c859c552c3418bb3701eaf773f77829e40
saHel <qaz74159@yahoo.com.tw>
2020-09-21 13:39:40 +0800

    transform the about page

--- alcpt/jinja2.py
+++ alcpt/jinja2.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 from jinja2 import Environment
 
+from .templatetags.web_filters import *
+
 
 def environment(**options):
     env = Environment(**options)
@@ -10,4 +12,16 @@ def environment(**options):
         'static': static,
         'url': reverse,
     })
+
+    # custom filters
+    env.filters["has_perm"] = has_perm
+    env.filters["linebreaksbr"] = linebreaksbr
+    env.filters["unread_count"] = unread_count
+    env.filters[
+        "get_TBManager_report_notification"] = get_TBManager_report_notification
+    env.filters[
+        "get_TestManager_report_notification"] = get_TestManager_report_notification
+    env.filters[
+        "get_SystemManager_report_notification"] = get_SystemManager_report_notification
+
     return env
