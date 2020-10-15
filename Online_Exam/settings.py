7f5fe8444736c9450b29441640b0eb6164fea067
Emily <mayou66321@gmail.com>
2020-10-15 12:40:22 +0800

    zh-translation 10/15

--- Online_Exam/settings.py
+++ Online_Exam/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ALCPT',
         'USER': 'root',
-        'PASSWORD': 'alcptalcpt',
+        'PASSWORD': 'emily310531',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
