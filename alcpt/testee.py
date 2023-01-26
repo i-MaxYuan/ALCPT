2dbb62e4e80ba4c8ba6b5f37ea1e677bb6718b3c
xji6u4tim <xji6u4tim@gmail.com>
2023-01-26 03:17:42 -0800

    lib_create

--- alcpt/testee.py
+++ alcpt/testee.py
@@ -1092,4 +1092,17 @@ def word_library(request):
         return render(request,'testee/word_library.html',{'word_list':word_list})
 
 def word_library_create(request):
+     if request.method == 'POST':
+        word_chinese = request.POST.get('word_chinese')
+        try:
+            word_english = request.POST.get('word_english')
+            Word_library.objects.get(words = word_chinese)
+            messages.error(request, "Failed created, word had been used - {}".format(word_english))
+            return redirect('word_library_create')
+        #Words 找不到同樣word_english才建立
+        except:
+
+            word_english = request.POST.get('word_english')
+            words = Word_library.objects.create(words = word_english,
+                                       translation = word_chinese)
         return render(request,'testee/word_library_create.html')
