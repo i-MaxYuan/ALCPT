1adc6b585fb06ab06bfa9cd0171b8a1c3aaedf7a
w1MmiNg <tetet2@protonmail.com>
2023-03-21 14:21:12 +0800

    word_library edit2

--- alcpt/testee.py
+++ alcpt/testee.py
@@ -1116,3 +1116,21 @@ def word_library_del(request,words):
         messages.error(request,'error')
         return redirect('word_library')
     
+def word_library_edit(request,words,translations):
+    word = Word_library.objects.get(words=words)
+    translate = Word_library.objects.get(translations=translations) 
+    if request.method == 'POST':
+        try:
+            word_english=request.POST.get('word_english')
+            word_chinese=request.POST.get('word_chinese')
+            word.words = word_english
+            translate.translations = word_chinese
+            word.save()
+            translate.save()
+            return redirect('word_library')
+        except ObjectDoesNotExist:
+            messages.error(request,'error')
+            return redirect('word_library')
+            
+    else:
+        return render(request,'testee/word_library_edit.html',{'words':word,'translates':translate})        
