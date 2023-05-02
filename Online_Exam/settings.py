9dc0e46ce29d0824d00367ed8b7a01fa581b44d8
Ting13035 <tina1641@gmail.com>
2023-05-02 10:50:30 +0800

    language

--- Online_Exam/settings.py
+++ Online_Exam/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-LOGIN_REDIRECT_URL = '/proclamation'
+LOGIN_REDIRECT_URL = '/zh/proclamation'
 LOGOUT_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'alcpt.User'
